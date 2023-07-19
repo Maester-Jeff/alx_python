6ececc32cb1742cd877cc1080b72ae6590cc7c90
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-20 02:33:32 +0300

    6-print_comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,6 @@
 for a in range(10):
     for b in range(a+1,10):
-        print("{}{}".format(a,b),end=", ")
-    if a == 8:
-        print("{}{}".format(a,b),end="\n")
+      if a < 8:
+        print("{}{},".format(a,b),end=" ")
+      else:
+        print("{}{}".format(a,b),end=" ")

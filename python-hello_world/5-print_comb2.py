57fd2388d86b480ec2e45f776e8ab3ab1373ca80
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-20 02:41:57 +0300

    5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,19 +1,9 @@
-
-def getSortedNumber(number):
-    number = sorted(number)
-    number = int(str(number))
-for number in range(100):
-    print("{:02}".format(number),end=", ")
-if number == 99:
-    print("{:02}".format(number),end="")
-
-'''
 for number in range(100):
   if number == 99:
-    print("{:02}".format(number),end=" ")
+    print("{:02}".format(number),end="\n")
   else:
     print("{:02}".format(number),end=", ")
-'''
+
     
 
     

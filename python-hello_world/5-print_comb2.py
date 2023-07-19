4681f2d836a4e3d65c03ed742bc80a4f9e7f9ed5
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-20 00:17:01 +0300

    5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -9,12 +9,10 @@ if number == 99:
 '''
 
 for number in range(100):
-  if number == 99:
-    print("{:02}".format(number),end=" ")
-  else:
+  if number < 99:
     print("{:02}".format(number),end=", ")
-    
-
+  else:  
+    print("{:02}".format(number),end=" ")
     
 
 

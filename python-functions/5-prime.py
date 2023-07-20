fbdeb4a814acd63c02ee3540c1b455eddc0f403b
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-21 01:20:07 +0300

    5-prime.py

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,11 @@
 def is_prime(number):
-    if number < 2:
+  if number < 2:
+    return False
+  for i in range(2, int(number ** 0.5) + 1):
+    if number % i == 0:
       return False
-    for i in range(2, int(number ** 0.5) + 1):
-      if number % i == 0:
-        return False
-    else:
-      return True
+  else:
+    return True
+
         
         

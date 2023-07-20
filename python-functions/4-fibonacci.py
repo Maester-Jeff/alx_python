a90915503b8e7859ce08c93d8a8047e7e604ad81
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-21 01:18:14 +0300

    5-prime.py

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,16 +1,11 @@
-def fibonacci_sequence(n):
-  fib_terms = n
-  counter = 0
-  term_1 = 0
-  term_2 = 1
-  next_term = 0
-  while counter <= fib_terms:
-    next_term = term_1 + term_2
-    term_1 = term_2
-    term_2 = next_term
-    counter = counter + 1
+def fib(n):
+  if n == 0:
+    return []
+  elif n == 1:
+    return [0]
+  else:
+    return [fib(n-1) + fib(n-2)]
 
-
-    
+print(fib(5))
 
 

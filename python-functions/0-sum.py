63fa72bf4acfe14434629a739c7d87a113e94d88
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-20 20:51:03 +0300

    0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,4 +1,9 @@
 def add(a,b):
   output = a + b
   return(output)
-print(add(1, 2))
+result_1 = add(1, 2)
+print(result_1)
+result_2 = add(98, 0)
+print(result_2)
+result_3 = add(100, -2)
+print(result_3)

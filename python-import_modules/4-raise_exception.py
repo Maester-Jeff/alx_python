d47092616b5026f37498a1304dd8598f61a1c946
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-26 20:35:27 +0300

    4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,28 +1,6 @@
-
 def raise_exception():
-  try:
-    raise_exception()
-  except TypeError as te:
-    print("Exception raised")
-
-'''
-def safe_print_division(a, b):
-  """
-  Divides 2 integers and prints the result.
-  Args:
-    a: The first integer to divide.
-    b: The second integer to divide by.
-  Returns:
-    The value of the division, otherwise: None
-  Raises:
-    ZeroDivisionError: If b is 0.
-  """
-  try:
-    result = a / b
-  except ZeroDivisionError:
-    print("Division by zero")
-  finally:
-    print("Inside result: {}".format(result))
-  return result
-print(safe_print_division(2, 0))
-'''
+  raise TypeError()
+try:
+  raise_exception()
+except TypeError as te:
+  print("Exception is raised")

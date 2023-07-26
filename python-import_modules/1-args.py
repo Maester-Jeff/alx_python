eb524b7de8e9f2c9cd3225b093de23e90f40a1a2
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 02:31:22 +0300

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,12 @@
 import sys
+def highlight_list(*argv):
+  if len(argv) == 0:
+    print(len(argv), "arguments.")
+  elif len(argv) == 1:
+    print(len(argv), "argument:")
+  else:
+    print(len(argv), "arguments:")
+  for i, arg in enumerate(argv, 1):
+      print(f"{i}: {arg}")
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    def highlight_list(*argv):
-        if len(argv) == 0:
-            print(len(argv), "arguments.")
-        elif len(argv) == 1:
-            print(len(argv), "argument:")
-        else:
-            print(len(argv), "arguments:")
-            for i, arg in enumerate(argv, 1):
-                print(f"{i}: {arg}")
+  argv = sys.argv[1:]

07887e2791912c502abd2fc75054a68e6d401e19
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 15:06:50 +0300

    2-multiple_returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
   length = len(sentence)
   first = sentence[0]
   print("Length: {:d} - First character: {}".format(length, first))
-multiple_returns("hello world")

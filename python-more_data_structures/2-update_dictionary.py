ea18dbb86056b39a9f4ebd70a469c505eb69e872
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 21:17:22 +0300

    2-update_dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-'''
-def update_dictionary(a_dicitonary, key, value):
-    a_dicitonary[key] = value
-'''
 def update_dictionary(a_dictionary, key, value):
-  """
-  Replaces or adds key/value in a dictionary.
-  Args:
-    a_dictionary: The dictionary to be updated.
-    key: The key to be updated or added.
-    value: The value to be associated with the key.
-  Returns:
-    The updated dictionary.
-  """
-  if key in a_dictionary:
-    a_dictionary[key] = value
-  else:
+  for key in a_dictionary:
     a_dictionary[key] = value
   return a_dictionary

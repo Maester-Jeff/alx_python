b304ab0edfd332ac4d663ebd90ad195ad03378e8
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 21:29:20 +0300

    2-update_dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-  for key in a_dictionary:
+  if key in a_dictionary:
     a_dictionary[key] = value #key exits value is replaced
   else:
     a_dictionary[key] = value #where its not available then the key is created

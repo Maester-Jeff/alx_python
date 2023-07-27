8c5bfd820b4ea76775860210ded12274b862e423
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 15:34:29 +0300

    1-print_matrix_integer

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,21 @@
+'''
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
   for row in matrix:
     for matrix_num in row:
       print("{:d}".format(matrix_num),end=' ')
+    print()
+'''
+
+def print_matrix_integer(matrix=[[]]):
+  """
+  Prints a matrix of integers.
+  Args:
+    matrix: A list of lists of integers.
+  Returns:
+    None.
+  """
+  for row in matrix:
+    for column in row:
+      print(str(column), end=" ")
     print()

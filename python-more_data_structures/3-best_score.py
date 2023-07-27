7c44fb3c6cbfcc81ab681ede85d490292a036516
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 21:59:09 +0300

    3-best_score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-  best_key = None
   best_score = None
+  best_key = None
   for key, value in a_dictionary.items():
     if best_score == None or value > best_score:
       best_score = value

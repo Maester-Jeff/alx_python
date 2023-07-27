f1eeb246fc4a38ae6b3ecb22ac698238a897eb55
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-27 03:28:17 +0300

    1-print_matrix_integer

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,4 @@
 def print_matrix_integer(matrix=[[]]):
-  transposed_matrix = list(zip(*matrix))
-  for row in transposed_matrix:
-    for num in row:
-      print("{:d}".format(num))
+  for row in matrix:
+    for matrix_num in row:
+      print("{:d}".format(matrix_num))

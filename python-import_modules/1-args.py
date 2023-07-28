d851271dfcce5abc64bfa4ccbcecc6853210accb
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-07-28 14:17:21 +0300

    1-args

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
   def highlight_list(*argv):
     num_argue = len(argv)
     if num_argue == 0:
-      print(num_argue, "arguments.")
+      print(num_argue, "arguments.",end="\n")
     elif num_argue == 1:
-      print(num_argue, "argument:")
+      print(num_argue, "argument:",end="\n")
     else:
-      print(num_argue, "arguments:")
+      print(num_argue, "arguments:",end="\n")
     for i, arg in enumerate(argv, 1):
-      print(f"{i}: {arg}", sep=" ")
+      print(f"{i}: {arg}")
 highlight_list()

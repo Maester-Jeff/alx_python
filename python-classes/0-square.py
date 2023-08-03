ff9dc5d367d03cde8268ab3279772c4657b3d376
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 15:45:00 +0300

    0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,9 +2,8 @@
 '''
 This program has a class square that defines a square with a private instance attribute named size
 '''
-''' delcaration of the class'''
 class Square:
-    '''defining and initializing the square object with a size arguement'''
+    ''' delcaration of the class'''
     def __init__(self, size):
-        '''making the size attribute private'''
+        '''defining and initializing the square object with a private size attribute'''
         self.__size = size     

cde0b6e908385ced5cd20f655a5723d8f166662a
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 20:52:30 +0300

    2-inheritance_from

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+The function returns true if the object is an isntance of a class directly or indirectly inheriting the specified class.
+'''
+def inherits_from(obj, a_class):
+    '''
+    Else it returns false.
+    '''
+    return inherits_from(obj, a_class)
+

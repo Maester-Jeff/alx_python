d14275491897e58fa16adfe01ed585b5e2149173
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 22:46:31 +0300

    4-base_geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -6,6 +6,17 @@ class BaseGeometry:
     '''
     creating the Class name.
     '''
+    def __dir__(cls) -> None:
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        attributes = super().__dir__()
+        list_to_return = []
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                list_to_return.append(attr)
+        return list_to_return
+    
     def area(self, area):
         '''Method raises the exception message as stated below.'''
         self.area = area

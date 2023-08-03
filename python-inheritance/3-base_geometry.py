4a6c3e83e079c525b0da88d411c8baea669c3bed
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 22:57:23 +0300

    3-base_geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,16 +4,11 @@ class BaseGeometryMetaClass(type):
     '''
     creating the meta Class to remove unwanted subclasses.
     '''
-    def __dir__(cls) -> None:
+    def __dir__(cls):
         '''
         function method creats a list of all attributes for the class and excludes the init_subclass.
         '''
-        attributes = super().__dir__()
-        list_to_return = []
-        for attr in attributes:
-            if attr != "__init_subclass__":
-                list_to_return.append(attr)
-        return list_to_return
+        return [attribute for attribute in super().__dir__() if attribute != '__innit_subclass__']
 class BaseGeometry(metaclass = BaseGeometryMetaClass):
     '''empty class created'''
     pass

63bdeaf9fc017a8131817887606c141dde17b6f7
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 23:01:28 +0300

    3-base_geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -11,4 +11,9 @@ class BaseGeometryMetaClass(type):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 class BaseGeometry(metaclass = BaseGeometryMetaClass):
     '''empty class created'''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     pass

33911db3fccb61c3d5c3d6d71015416841ef0016
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 21:59:51 +0300

    2-inherits_from

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,4 +6,4 @@ The function returns true if the object is an isntance of a class directly or in
 
 def inherits_from(obj, a_class):
     '''Else it returns false.'''
-    return issubclass(type(obj), a_class) and type(obj)!=a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class

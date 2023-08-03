ccf8cb3f39ebab3391fdf30211d6d5af751e88b8
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 21:07:03 +0300

    2-inherits_from

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,4 +6,6 @@ def inherits_from(obj, a_class):
     '''
     Else it returns false.
     '''
-    return isinstance(obj, type) and issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
+
+

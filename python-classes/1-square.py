d85798849d2279162e876ddad0e1fa81f951cb74
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 15:18:52 +0300

    1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,5 +8,10 @@ class Square:
     def __init__(self, size=0):
         '''making the size attribute private'''
         self.__size = size
+    '''Creating the loop for checking whether the size is an integer and not less than zero'''
     def check_size(self):
-        pass
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+

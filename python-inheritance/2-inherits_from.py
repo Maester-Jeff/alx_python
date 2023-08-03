dbf1e7eae4c4f0a50638ab00a3bc31a9d20e4e95
Maester-Jeff <135169680+Maester-Jeff@users.noreply.github.com>
2023-08-03 22:45:41 +0300

    2-inherits_from

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-'''
-The function returns true if the object is an isntance of a class directly or indirectly inheriting the specified class.
-'''
+'''module creates a function that checks instance of an object'''
 
 
 def inherits_from(obj, a_class):
-    '''Else it returns false.'''
+    '''
+    The function returns true if the object is an isntance of a class directly or indirectly inheriting the specified class.Else it returns false.
+    '''
     return issubclass(type(obj), a_class) and type(obj) != a_class

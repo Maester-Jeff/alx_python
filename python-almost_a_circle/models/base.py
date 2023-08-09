0e9105daad7ecf99ba4efb120f5c533892ac86dd
Maester-jeff <jeffndungu45@gmail.com>
2023-08-09 22:53:59 +0300

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-'''This program creates a class base with the argument and function statement below'''
+'''
+This program creates a class base with the 
+argument and function statement below.
+'''
 class Base:
+    '''creation of class base.'''
     __nb_objects = 0
     def __init__(self, id=None):
-        '''The init function initialzing the object'''
+        '''The init function initialzing the object.'''
         self.id = id
         if id is not None:
-            '''if statement checking whether the id attribute is none'''
+            '''if statement checking whether the id attribute is none.'''
             self.id = id
         else:
             Base.__nb_objects += 1

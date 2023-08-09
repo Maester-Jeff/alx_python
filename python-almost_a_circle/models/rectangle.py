bee6247e3207c24ef9607c3b5b0fa843e354fe25
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 01:29:31 +0300

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ This script creates class that inherits the previous base class.
 from models.base import Base
 '''importing the script of the previous parent class.'''
 
-class rectangle(Base):
+class Rectangle(Base):
     '''creation and initialization of the inheriting class.'''
     def __init__(self, width, height, x=0, y=0, id=None):
         '''class constructor with the necessary attributes.'''

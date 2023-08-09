9bdc2a2c0eb09aad343711160df55cb18823aa4a
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 01:40:32 +0300

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,30 +19,46 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''The getter method for width attribute'''
+        '''The getter method for width attribute.'''
         return self.__width
     @width.setter
     def width(self, value):
-        '''The setter method for width attribute'''
+        '''The setter method for width attribute.'''
         self.__width = value
+        '''if statement validating width.'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <=0:
+            raise ValueError("width must be > 0")
 
     @property
     def height(self):
-        '''The getter method for height attribute'''
+        '''The getter method for height attribute.'''
         return self.__height
     @height.setter
     def height(self, value):
-        '''The setter method for height attribute'''
+        '''The setter method for height attribute.'''
         self.__height = value
+        '''If statement validating height.'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
 
     @property
     def x(self):
-        '''The getter method for x attribute'''
+        '''The getter method for x attribute.'''
         return self.__x
     @x.setter
     def x(self, value):
         '''The getter method for x attribute'''
         self.__x = value
+        '''If statement validating x.'''
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+    
     
     @property
     def y(self):
@@ -52,3 +68,8 @@ class Rectangle(Base):
     def y(self, value):
         '''The getter method for y attribute'''
         self.__y = value
+        '''If statement validating y.'''
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")

79b051535d85c8ae4853b4be32008d443eba87f8
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 01:44:11 +0300

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,13 +24,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''The setter method for width attribute.'''
-        self.__width = value
         '''if statement validating width.'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value <=0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-
+        self.__width = value
     @property
     def height(self):
         '''The getter method for height attribute.'''

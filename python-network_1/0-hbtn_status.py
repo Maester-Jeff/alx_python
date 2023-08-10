5094b92b239380620cd1a3e13fb7644052a23d77
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 21:59:33 +0300

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,5 +2,6 @@
 '''Script that fetches a URL.'''
 import requests
 '''Importing requests package.'''
-req = requests.get('https://alu-intranet.hbtn.io/status')
-print("Body response:       \n -type: {} \n -content: {}".format(type(req.text), req.text))
+if __name__ == "__main__":
+    req = requests.get('https://alu-intranet.hbtn.io/status')
+    print("Body response:\n\t-type: {}\n\t-content: {}".format(type(req.text), req.text))

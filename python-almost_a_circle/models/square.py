a787831523dbb8293737e2fcdc0d6e7e02650f23
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 16:04:51 +0300

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,24 +12,18 @@ class Square(Rectangle):
         Class constructor initialized based on rectangle attributes. 
         Width and height replaced with size.
         '''
-        self.width = size
-        self.height = size
-        self.x = size
-        self.y = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         '''Getter method for the size attribute to represent both width and height.'''
-        return self.height, self.y
+        return self.width
     
     @size.setter
     def size(self, value):
         '''Corresponding setter method for size.'''
         self.width = value
         self.height = value
-        self.x = value
-        self.y = value
 
     def __str__(self):
         '''Overloading string method.'''

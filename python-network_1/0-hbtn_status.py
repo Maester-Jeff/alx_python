fc9c1aca0271fa704d834e1e7c10df6daa372f55
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 22:01:03 +0300

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,4 +4,4 @@ import requests
 '''Importing requests package.'''
 if __name__ == "__main__":
     req = requests.get('https://alu-intranet.hbtn.io/status')
-    print("Body response:\n\t-type: {}\n\t-content: {}".format(type(req.text), req.text))
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text), req.text))

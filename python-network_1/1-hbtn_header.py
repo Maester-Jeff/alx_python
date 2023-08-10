65d5e8503d3681cb520316b7033cfed78b4ee14a
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 22:20:19 +0300

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''Python scripts that take a URL and sends a request.'''
-import os
-'''Importing os module.'''
+import sys
+'''Importing sys module.'''
 import requests
 '''Impporting requests package.'''
 if __name__ == "__main__":
     '''Code should not be executed when imported.'''
-    req = requests.get('')
+    url = sys.argv[1]
+    req = requests.get(url)
     print(req.headers['X-Request-Id'])

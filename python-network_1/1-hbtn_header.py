4860e30ae73f23a2c5a0500df623bfa8cce6f3f6
Maester-jeff <jeffndungu45@gmail.com>
2023-08-11 00:49:18 +0300

    1-hbtn

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,3 @@
-'''
 #!/usr/bin/python3
 '''Python scripts that take a URL and sends a request.'''
 import requests
@@ -9,18 +8,5 @@ if __name__ == "__main__":
     '''Code should not be executed when imported.'''
     url = sys.argv[1]
     req = requests.get(url)
-    response = req.headers['X-Request-Id']
+    response = req.headers.get('X-Request-Id')
     print(response)
-'''
-import requests
-import sys
-# Get the URL from the command line arguments.
-url = sys.argv[1]
-# Create a request object using the requests.get() function.
-request = requests.get(url)
-# Send the request and get the response.
-response = request.content
-# Get the value of the X-Request-Id variable from the response header.
-x_request_id = response.headers.get('X-Request-Id')
-# Print the value of the X-Request-Id variable.
-print(x_request_id)

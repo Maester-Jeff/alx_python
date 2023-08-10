00ae8e48b657a55ed17cbd7ac1aa7a66219df204
Maester-jeff <jeffndungu45@gmail.com>
2023-08-11 02:05:14 +0300

    5-json)api.py

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -12,16 +12,15 @@ if __name__ == "__main__":
     '''Code should not be executed when imported.'''
     if len(sys.argv) == 2:
         '''Checking if argument is given.'''
-        letter = sys.argv[1]
+        q = sys.argv[1]
     else:
-        letter = ""
+        q = ""
     url = "http://0.0.0.0:5000/search_user"
-    letter_parameter = {"q": letter}
+    letter_parameter = {"q": q}
     response = requests.post(url, data = letter_parameter)
     try:
         '''Checking if repsonse is JSON formatted.'''
         json_response = response.json()
-        
         if isinstance(json_response, dict) and json_response:
             user_id = json_response.get("id")
             user_name = json_response.get("name")

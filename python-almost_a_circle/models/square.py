44f5d768866a68c15eb9618ef79a6fa48411f54e
Maester-jeff <jeffndungu45@gmail.com>
2023-08-10 15:53:45 +0300

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
 '''
 This script involves class square inheriting the previous rectangle class.
 '''
+from models.rectangle import Rectangle
+'''Importing the previous rectangle script.'''
+
 class Square(Rectangle):
     '''Class square created.'''
     def __init__(self, size, x=0, y=0, id=None):
@@ -13,15 +15,18 @@ class Square(Rectangle):
         self.width = size
         self.height = size
         super().__init__(size, size, x, y, id)
+
     @property
     def size(self):
         '''Getter method for the size attribute to represent both width and height.'''
         return self.height, self.width
+    
     @size.setter
     def size(self, value):
         '''Corresponding setter method for size.'''
         self.width = value
         self.height = value
+
     def __str__(self):
         '''Overloading string method.'''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"

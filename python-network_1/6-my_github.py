66168896fa843a975bd36698572631589ba0df57
Maester-jeff <jeffndungu45@gmail.com>
2023-08-11 02:32:54 +0300

    6-my_github.py

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
+'''
+import requests
+'''Importing requests module.'''
+import sys
+'''Impporting sys package.'''
+if __name__ == "__main__":
+    '''Code should not be executed when imported.'''
+    user_name = sys.argv[1]
+    user_password = sys.argv[2]
+    url = "https://api.github.com/user"
+    auth = requests.auth.HTTPBasicAuth(user_name, user_password)
+    response = requests.get("https://api.github.com/user", auth = auth)
+    user_info = response.json()
+    user_id = user_info.get("id")
+    if response.status_code == 200:
+        print(user_id)
+    else:
+        print("Error code:", response.status_code)

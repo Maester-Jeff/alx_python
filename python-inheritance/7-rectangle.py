d2154ed44638e365f8da9e07e759215498c74467
Maester-jeff <jeffndungu45@gmail.com>
2023-08-11 03:24:39 +0300

    7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -42,4 +42,5 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
     def area(self):
         return self._width * self.__height
-    
+    def __str__(self):
+        return f'[Rectangle] {self.__width}/{self.__height}'

0fd6483fa58270153ba3dd27a2ade522901cebc7
Maester-jeff <jeffndungu45@gmail.com>
2023-08-24 23:49:47 +0300

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,43 @@
+import MySQLdb
+import sys
+
+def list_cities_by_state(username, password, database, state_name):
+        # Connect to the MySQL server
+        dbase = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        
+        # Create a cursor to interact with the database
+        cursor = dbase.cursor()
+        
+        # Prepare the SQL query with parameters
+        query = "SELECT cities.id, cities.name, states.name \
+                 FROM cities \
+                 JOIN states ON cities.state_id = states.id \
+                 WHERE states.name = %s \
+                 ORDER BY cities.id ASC"
+        
+        # Execute the SQL query with the provided state_name parameter
+        cursor.execute(query, (state_name,))
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        
+        # Display the results
+        for row in rows:
+            print(row)
+        
+        # Close the cursor and connection
+        cursor.close()
+        dbase.close()
+
+if __name__ == "__main__":
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+        list_cities_by_state(username, password, database, state_name)

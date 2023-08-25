70d6b6a4509d540e4571ebd3177ea894a07bc74b
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 17:23:22 +0300

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -3,21 +3,18 @@ import sys
 import MySQLdb
 
 def list_cities(username, password, dbname):
-    try:
         # Connect to the MySQL server
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbname)
+        dbase = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbname)
 
         # Create a cursor to interact with the database
-        cursor = db.cursor()
+        cursor = dbase.cursor()
 
         # Execute the query to fetch cities along with their state names
-        query = """
-        SELECT cities.id, cities.name, states.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id ASC;
-        """
-        cursor.execute(query)
+        results = "SELECT cities.id, cities.name, states.name \
+                FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id ASC"
+        cursor.execute(results)
 
         # Fetch all the results
         results = cursor.fetchall()
@@ -28,19 +25,10 @@ def list_cities(username, password, dbname):
 
         # Close the cursor and the database connection
         cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+        dbase.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <dbname>".format(sys.argv[0]))
-        sys.exit(1)
-
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-
     list_cities(username, password, dbname)

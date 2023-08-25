fd2041fb1072c221c054c66ea39e2c52311acf54
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 19:43:24 +0300

    6-model

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -8,8 +8,8 @@ from sqlalchemy import create_engine
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <dbname>".format(sys.argv[0]))
-        sys.exit(1)
 
+        sys.exit(1)
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)

229f9a1c0707d9ff67b3c6ef1d9bb287cbc99fa8
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 17:30:02 +0300

    5-filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,3 +1,4 @@
+'''
 import MySQLdb
 import sys
 
@@ -41,3 +42,53 @@ if __name__ == "__main__":
         database = sys.argv[3]
         state_name = sys.argv[4]
         list_cities_by_state(username, password, database, state_name)
+'''
+
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+def list_cities_by_state(username, password, dbname, state_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbname)
+
+        # Create a cursor to interact with the database
+        cursor = db.cursor()
+
+        # Execute the query to fetch cities of the specified state
+        query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC;
+        """
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the results
+        results = cursor.fetchall()
+
+        # Display the results as a comma-separated string
+        cities_list = ', '.join([row[0] for row in results])
+        print(cities_list)
+
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <dbname> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    list_cities_by_state(username, password, dbname, state_name)

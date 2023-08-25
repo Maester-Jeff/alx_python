8a9fe5d947c7507ae2b28de295977d1ec231fc35
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 16:47:29 +0300

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,36 +1,20 @@
+
 import MySQLdb
-import sys
+
 
 def list_cities(username, password, database):
-        # Connect to the MySQL server
-        dbase = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        
-        # Create a cursor to interact with the database
-        cursor = dbase.cursor()
-        
-        # Execute the SQL query to retrieve all cities and sort by id
-        results = "SELECT * FROM cities ORDER BY id ASC"
-        cursor.execute(results)
-        
-        # Fetch all the rows
-        rows = cursor.fetchall()
-        
-        # Display the results
-        for row in rows:
+    try:
+        conn = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+        results = cursor.fetchall()
+        for row in results:
             print(row)
-        
-        # Close the cursor and connection
         cursor.close()
-        dbase.close()
+        conn.close()
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database: {}".format(e))
 
-if __name__ == "__main__":
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        list_cities(username, password, database)
+
+if __name__ == '__main__':
+    list_cities('username', 'password', 'database_name')

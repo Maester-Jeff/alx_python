fe6f3719511deae0e66dfe4755278048aab7700d
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 17:17:12 +0300

    4-cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,4 @@
-import MySQLdb
+'''import MySQLdb
 import sys
 
 def list_cities(username, password, database):
@@ -16,7 +16,10 @@ def list_cities(username, password, database):
         cursor = dbase.cursor()
         
         # Execute the SQL query to retrieve all cities and sort by id
-        results = "SELECT * FROM cities ORDER BY id ASC"
+        results = "SELECT * FROM cities ORDER BY id ASC" ELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC;
         cursor.execute(results)
         
         # Fetch all the rows
@@ -35,3 +38,30 @@ if __name__ == "__main__":
         password = sys.argv[2]
         database = sys.argv[3]
         list_cities(username, password, database)
+'''
+
+import MySQLdb
+
+
+def list_cities(username, password, database):
+        dbase = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username, 
+            passwd=password, 
+            db=database
+        )
+        cursor = dbase.cursor()
+        results = "SELECT cities.id, cities.name, states.name \
+                FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id ASC"
+        cursor.execute(results)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        dbase.close()
+
+if __name__ == '__main__':
+    list_cities('username', 'password', 'database_name')

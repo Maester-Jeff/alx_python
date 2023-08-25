7be735d1f9bef58a307d9436465e50f2610a2fcd
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 16:58:16 +0300

    4-cities

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,43 @@
-
 import MySQLdb
-
+import sys
 
 def list_cities(username, password, database):
-        conn = MySQLdb.connect(
-            host="localhost", 
-            port=3306, 
-            user=username, 
-            passwd=password, 
+    try:
+        # Connect to the MySQL server
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
             db=database
         )
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
-        results = cursor.fetchall()
-        for row in results:
+        
+        # Create a cursor to interact with the database
+        cursor = connection.cursor()
+        
+        # Execute the SQL query to retrieve all cities and sort by id
+        query = "SELECT * FROM cities ORDER BY id ASC"
+        cursor.execute(query)
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        
+        # Display the results
+        for row in rows:
             print(row)
+        
+        # Close the cursor and connection
         cursor.close()
-        conn.close()
+        connection.close()
+        
+    except MySQLdb.Error as e:
+        print("Error:", e)
 
-if __name__ == '__main__':
-    list_cities('username', 'password', 'database')
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_cities(username, password, database)

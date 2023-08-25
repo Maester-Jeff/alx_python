45572c9356c94df8752c3c97fa70dae783cba442
Maester-jeff <jeffndungu45@gmail.com>
2023-08-25 19:40:45 +0300

    6-model

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
         print("Usage: {} <username> <password> <dbname>".format(sys.argv[0]))
 
+
         sys.exit(1)
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)

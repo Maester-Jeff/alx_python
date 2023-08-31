b1a5291803d4ed87b9167ee64620a2a5b334d7c2
Maester-jeff <jeffndungu45@gmail.com>
2023-08-31 20:48:42 +0300

    4-number_route.py

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -26,7 +26,7 @@ def python(text):
     text = text.replace("_", " ")
     return f"Python {text}"
 
-@app.route("/number/<int: n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     if n is int:
         return f'{n} is a number'

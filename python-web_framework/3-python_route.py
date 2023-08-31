f4a2c921f08a02be489caa1356078be02ea486a9
Maester-jeff <jeffndungu45@gmail.com>
2023-08-31 20:25:49 +0300

    3-python_route.py

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -20,8 +20,9 @@ def c(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
+@app.route("/python", defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
-def python(text='is_cool'):
+def python(text):
     text = text.replace("_", " ")
     return f"Python {text}"
 

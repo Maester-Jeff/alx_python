7b1ce32a3063b940325ff0d66edb3649767d93c2
Maester-jeff <jeffndungu45@gmail.com>
2023-08-31 23:13:15 +0300

    5

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -31,7 +31,7 @@ def number(n):
     return f'{n} is a number'
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def template(n):
+def temp(n):
     return render_template("5-number.html", n=n)
 
 if __name__ == "__main__":

60428617849e3d96fe75346456eaa353cf761173
Maester-jeff <jeffndungu45@gmail.com>
2023-08-31 23:27:45 +0300

    5-number_template.py

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -35,4 +35,4 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0",port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)

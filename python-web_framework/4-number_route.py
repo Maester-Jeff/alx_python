7cb4afc5ca724b4549884802e1bb79e9f8fc6a7c
Maester-jeff <jeffndungu45@gmail.com>
2023-08-31 20:39:45 +0300

    4-number_route.py

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application.
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+   return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    if n is int:
+        return f'n is a number'
+    else:
+        return None
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
